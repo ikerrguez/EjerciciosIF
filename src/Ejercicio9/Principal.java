17bd782ece7440840f5f5df0a4b7773ab46ad766
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-02 13:17:57 +0200

    Ejercicio 9 sin acabar

---
+++ src/Ejercicio9/Principal.java
@@ -0,0 +1,18 @@
+package Ejercicio9;
+
+import java.util.Scanner;
+
+public class Principal {
+    static void main() {
+        System.out.println("Introduce tres números enteros diferentes");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el primer número");
+        int numero1 = sc.nextInt();
+        System.out.println("Introduce el segundo número");
+        int numero2 = sc.nextInt();
+        System.out.println("Introduce el tercer número");
+        int numero3 = sc.nextInt();
+        
+
+    }
+}
