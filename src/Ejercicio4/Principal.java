bcae1e475d99e5d8693526e753601fcf67e167ca
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-02 10:16:29 +0200

    Ejercicio 4 acabado

---
+++ src/Ejercicio4/Principal.java
@@ -0,0 +1,34 @@
+package Ejercicio4;
+
+import java.util.Scanner;
+
+public class Principal {
+    static void main() {
+        System.out.println("Bienvenido al sistema.");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dime tu peso en kilogramos");
+        double peso = sc.nextDouble();
+        System.out.println("Dime tu altura en metros");
+        double altura = sc.nextDouble();
+        double IMC = peso / (altura * altura);
+        System.out.println("Tu imc es de : " + IMC);
+
+        if (IMC < 18.5) {
+            System.out.println("Tu peso es bajo");
+        } else if (IMC >= 18.5 && IMC <= 25) {
+            System.out.println("Tu peso es normal");
+        } else if (IMC >= 25 && IMC <= 30) {
+            System.out.println("Estás en sobrepeso");
+        }
+        if (IMC >= 30) {
+            System.out.println("Tienes obesiddad");
+        }
+
+}
+
+
+
+
+
+
+}
