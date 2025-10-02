be30f0111a989a2ef1bf1d1fc59247268230cfb5
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-02 10:44:27 +0200

    Ejercicio 5 acabado

---
+++ src/Ejercicio5/Principal.java
@@ -0,0 +1,21 @@
+package Ejercicio5;
+
+import java.util.Scanner;
+
+public class Principal {
+    static void main() {
+        System.out.println("Dime un año");
+        Scanner sc = new Scanner(System.in);
+        int anho = sc.nextInt();
+
+        if (anho % 4 == 0 || anho % 100 != 0 || anho % 400 == 0) {
+            System.out.println("El año es bisiesto");
+
+        } else if (anho * 4 == 0 || anho * 100 == 0 && anho * 400 != 0) {
+            System.out.println("El año no es bisiesto");
+        }
+
+    }
+
+}
+
