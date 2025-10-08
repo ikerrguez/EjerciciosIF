c2d7c1c8099f00d12498d058aa1d94de7d3a89b7
ikerrguez <ikerrodriguezgarcia07@liceolapaz.net>
2025-10-08 09:09:12 +0200

    Ejercicio prueba de bucle for

---
+++ src/prueba/Principal.java
@@ -0,0 +1,10 @@
+package prueba;
+
+public class Principal {
+    static void main() {
+        for (int i=0; i<=100; i+=2) {
+            System.out.println(i);
+        }
+        System.out.println("FIN");
+    }
+}
